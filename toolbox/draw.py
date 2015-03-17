78109410a5cb62ef752be3be082e13d7fec37a3e
Jerry Mickle <github@goatpox.net>
2015-03-17 17:27:45 +0000

    new functions
    added dist() to util.py, and circle() to draw.py

--- toolbox/draw.py
+++ toolbox/draw.py
@@ -1,3 +1,5 @@
+import util
+
 def rect(tilemap,tile,x,y,w,h,fill = 0):
 	if (fill == 0):
 		for i in range(x,x+w):
@@ -10,4 +12,9 @@ def rect(tilemap,tile,x,y,w,h,fill = 0):
 		for i in range(x,x+w):
 			for j in range(y,y+h):
 				tilemap.set_tile(i,j,tile)
-		
+
+def circle(tilemap,tile,x,y,radius):
+	for i in range(x-radius,x+radius):
+		for j in range(y-radius,y+radius):
+			if (util.dist(i,j,x,y)<radius):
+				tilemap.set_tile(i,j,tile)
--- util.py
+++ util.py
@@ -1,1 +1,3 @@
-print "util.py"
+import math
+def dist(x1,y1,x2,y2):
+	return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
