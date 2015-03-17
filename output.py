8017efa92a51cd2469ba9a368d536ee72dc12515
Jerry Mickle <github@goatpox.net>
2015-03-17 18:15:11 +0000

    more stuff
    hope it works!

--- output.py
+++ output.py
@@ -1,3 +1,4 @@
+import util
 try:
 	import omg
 except:
@@ -11,7 +12,20 @@ def to_image(tilemap):
 
 def to_map(tilemap):
 	print("output.to_map() not implemented")
-
+	#convert all tiles to square sectors
+	sectors = []
+	for i in range(tilemap.width):
+		for j in range(tilemap.height):
+			if (tilemap.get_tile(i,j) != ""):
+				sectors.append(util.Sect([(i,j),(i+32,j),(i+32,j+32),(i,j+32)]
+				
+	map = omg.mapedit.MapEditor()
+	
+	for s in sectors:
+		map.draw_sector(s)
+	
+	return map.to_lumps()
+	
 def to_print(tilemap):
 	print("printing tilemap")
 	for j in range(tilemap.height):
--- scripts/default.py
+++ scripts/default.py
@@ -1,6 +1,19 @@
+import random
+import toolbox.draw
 import toolbox.digger
 
 def run(tilemap):
-	toolbox.digger.dig(tilemap,"#",15,10)
-	toolbox.digger.dig(tilemap,"#",15,10)
-	toolbox.digger.dig(tilemap,"#",15,10)
+	toolbox.draw.circle(tilemap,".",random.randint(0,30),random.randint(0,20),random.randint(3,8))
+	toolbox.draw.circle(tilemap,".",random.randint(0,30),random.randint(0,20),random.randint(3,8))
+	toolbox.draw.circle(tilemap,".",random.randint(0,30),random.randint(0,20),random.randint(3,8))
+	toolbox.draw.circle(tilemap,".",random.randint(0,30),random.randint(0,20),random.randint(3,8))
+	toolbox.draw.circle(tilemap,".",random.randint(0,30),random.randint(0,20),random.randint(3,8))
+	toolbox.draw.circle(tilemap,".",random.randint(0,30),random.randint(0,20),random.randint(3,8))
+	toolbox.digger.dig(tilemap,"+",random.randint(0,30),random.randint(0,20))
+	toolbox.digger.dig(tilemap,"+",random.randint(0,30),random.randint(0,20))
+	x = random.randint(0,15)
+	y = random.randint(0,10)
+	w = random.randint(5,15)
+	h = random.randint(4,10)
+	toolbox.draw.rect(tilemap,"#",x,y,w,h,1)
+	#toolbox.draw.rect(tilemap,"",x-1,y-1,w+2,h+2,0)
