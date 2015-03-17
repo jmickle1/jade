590e1b662b55ebb40c4a1baed5df4587a4bb7e70
jmickle1 <jaymickle@gmail.com>
2015-03-17 23:56:41 +0000

    stupid fixes

--- output.py
+++ output.py
@@ -11,13 +11,12 @@ def to_image(tilemap):
 			print("output.to_image() not implemented")
 
 def to_map(tilemap):
-	print("output.to_map() not implemented")
 	#convert all tiles to square sectors
 	sectors = []
 	for i in range(tilemap.width):
 		for j in range(tilemap.height):
 			if (tilemap.get_tile(i,j) != ""):
-				sectors.append(util.Sect([(i,j),(i+32,j),(i+32,j+32),(i,j+32)]
+				sectors.append([(i,j),(i+32,j),(i+32,j+32),(i,j+32)])
 				
 	map = omg.mapedit.MapEditor()
 	
--- util.py
+++ util.py
@@ -3,6 +3,6 @@ def dist(x1,y1,x2,y2):
 	return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
 class Sect(object):
-		__init__(self,verts):
+		def __init__(self,verts):
 			self.verts = verts
 	
