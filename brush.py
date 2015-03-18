bc3f6d96805e769547326ad70a3f976ccb6d63f6
jmickle1 <jaymickle@gmail.com>
2015-03-18 17:27:18 +0000

    brush reorganized, output combines sectors
    in future i need to combine the sectors BEFORE i write them to the map,
    as it takes a huge amount of time to process, but this works correctly
    for now

--- brush.py
+++ brush.py
@@ -1,13 +1,14 @@
 import omg.mapedit
 
 class Brush(object):
-	def __init__(self,floor = "SLIME14",ceil = "CEIL1_2",texture = "METAL2",z_floor = 0,z_ceil = 128,light=160):
+	def __init__(self,floor = "SLIME14",ceil = "CEIL1_2",texture = "METAL2",z_floor = 0,z_ceil = 128,light=192):
 		self.floor 	= floor
 		self.ceil = ceil
 		self.texture = texture
 		self.z_floor = z_floor
 		self.z_ceil = z_ceil
 		self.light = light
+		self.sector = omg.mapedit.Sector(self.z_floor,self.z_ceil,self.floor,self.ceil,self.light)
 		
 	def to_sector(self):
-		return omg.mapedit.Sector(self.z_floor,self.z_ceil,self.floor,self.ceil,self.light)
+		return self.sector
--- output.py
+++ output.py
@@ -12,11 +12,27 @@ def to_map(tilemap):
 	#convert all tiles to square sectors
 	sectors = []
 	map = omg.mapedit.MapEditor()
+	print("output.py to_map(), drawing sectors")
 	for i in range(tilemap.width):
 		for j in range(tilemap.height):
 			if (tilemap.get_tile(i,j) != ""):
 				sec = tilemap.tileinfo[tilemap.get_tile(i,j)].to_sector()
 				map.draw_sector([(i*32,j*32),((i*32)+32,j*32),((i*32)+32,(j*32)+32),(i*32,(j*32)+32)],sec)
+				
+	print("output.py to_map(), cleaning up sectors")
+	combined = 0
+	for s1 in map.sectors:
+		comb_list = []
+		for s2 in map.sectors:
+			if (s1 != s2):
+				if (map.compare_sectors(s1,s2)):
+					if (s2 not in comb_list):
+						combined+=1
+						comb_list.append(s2)
+		for c in comb_list:
+			map.combine_sectors(s1,c)
+			
+	print("combined sectors: "+str(combined))
 	
 	return map.to_lumps()
 	
