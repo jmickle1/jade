a13f216ed78774fba48cfa7d7d2cba439fcdd867
jmickle1 <jaymickle@gmail.com>
2015-03-18 22:33:09 +0000

    added the zdbsp nodebuilder and added automatic nodebuilding to output

--- output.py
+++ output.py
@@ -1,6 +1,10 @@
 import util
 import omg
 import omg.mapedit
+import subprocess
+import os
+
+zdbsp_path = os.path.dirname(os.path.abspath(__file__)) + "/junk/zdbsp.exe"
 
 def to_image(tilemap):
 	data = tilemap.data
@@ -21,21 +25,27 @@ def to_map(tilemap):
 				map.draw_sector([(i*32,j*32),((i*32)+32,j*32),((i*32)+32,(j*32)+32),(i*32,(j*32)+32)],sec,sid)
 				
 	print("output.py to_map(), cleaning up sectors")
-	combined = 0
+	
 	for s1 in map.sectors:
 		comb_list = []
 		for s2 in map.sectors:
 			if (s1 != s2):
 				if (map.compare_sectors(s1,s2)):
 					if (s2 not in comb_list):
-						combined+=1
 						comb_list.append(s2)
 		for c in comb_list:
 			map.combine_sectors(s1,c)
-			
-	print("combined sectors: "+str(combined))
+		#print(str(map.sectors.index(s1))+"/"+str(len(map.sectors)))
 	
 	return map.to_lumps()
+	
+def to_file(tilemap,path):
+	wad = omg.WAD()
+	wad.maps["MAP01"] = to_map(tilemap)
+	wad.to_file(path)
+	print(zdbsp_path)
+	subprocess.call([zdbsp_path,"-o"+path,path])
+	
 	
 def to_print(tilemap):
 	print("printing tilemap")
