bc9e645ae3c7daabb0a6b27e659b76eb5f29df1c
jmickle1 <jaymickle@gmail.com>
2015-03-18 14:33:44 +0000

    Updated brush.py, now exports to omgifol sectors

--- brush.py
+++ brush.py
@@ -1,7 +1,13 @@
-print("brush.py")
+import omg.mapedit
 
 class Brush(object):
-	def __init__(self,floor,ceil,texture):
-		self.floor = floor
+	def __init__(self,floor = "SLIME14",ceil = "CEIL1_2",texture = "METAL2",z_floor = 0,z_ceil = 128,light=160):
+		self.floor 	= floor
 		self.ceil = ceil
 		self.texture = texture
+		self.z_floor = z_floor
+		self.z_ceil = z_ceil
+		self.light = light
+		
+	def to_sector(self):
+		return omg.mapedit.Sector(self.z_floor,self.z_ceil,self.floor,self.ceil,self.light)
