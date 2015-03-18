bb4d0926458ee4fc0377c4a626d1fe0090861d85
jmickle1 <jaymickle@gmail.com>
2015-03-18 10:10:27 +0000

    tilemap work

--- tilemap.py
+++ tilemap.py
@@ -1,10 +1,18 @@
+import omg.util
+
 class Tilemap(object):
 	def __init__(self, width, height):
 		self.width = width
 		self.height = height
 		self.data = [["" for y in range(height)] for x in range(width)]
+		self.tileinfo = omg.util.OrderedDict()
 	
 	def set_tile(self,x,y,tile):
+		try:
+			self.tileinfo[tile]
+		except:
+			print("you fucked up")
+			self.set_tile_sector(tile,(0,128,"BLOOD1","F_SKY1",160))
 		if (x >= 0 and y >= 0 and x < self.width and y < self.height):
 			self.data[x][y] = tile
 		else:
@@ -18,3 +26,6 @@ class Tilemap(object):
 		
 	def get_size(self):
 		return (self.width,self.height)
+	
+	def set_tile_sector(self,tile,sector):
+		self.tileinfo[tile] = sector
